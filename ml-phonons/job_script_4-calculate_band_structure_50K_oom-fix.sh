57b45c61f19f281d23ccf0ed7df36eb311bbf33e
Ouail Zakary <72498017+ozakary@users.noreply.github.com>
2026-03-23 12:45:16 +0200

    Update and rename job_script_4-calculate_band_structure_10K_oom-fix.sh to job_script_4-calculate_band_structure_50K_oom-fix.sh

--- ml-phonons/job_script_4-calculate_band_structure_50K_oom-fix.sh
+++ ml-phonons/job_script_4-calculate_band_structure_50K_oom-fix.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -l
 
 #--------------------------------------------------------------------------------------------------------------------------------
-#SBATCH --partition=standard
-#SBATCH --ntasks=1
-#SBATCH --time=16:00:00
-#SBATCH --account=project_462001159
-#SBATCH --output=calc_band_str_10K_%j.out
-#SBATCH --error=calc_band_str_10K_%j.err
+#SBATCH --partition=standard-g
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=40
+#SBATCH --gpus-per-node=1
+#SBATCH --time=24:00:00
+#SBATCH --account=<project_ID>
+#SBATCH --output=calc_band_str_50K_%j.out
+#SBATCH --error=calc_band_str_50K_%j.err
 #SBATCH --get-user-env
 #SBATCH --exclusive
 #SBATCH --hint=nomultithread
@@ -14,11 +16,11 @@
 
 module load cray-python
 
-source /scratch/project_462001159/zakaryou/packages/data_analysis_packages/data_analysis_env/bin/activate
+source /path/to/data_analysis_env/bin/activate
 
 # Set unlimited stack size
 ulimit -s unlimited
 
-python3 -u 4-calculate_band_structure_10K_oom-fix.py
+python3 -u 4-calculate_band_structure_50K_oom-fix.py
 
 exit 0
